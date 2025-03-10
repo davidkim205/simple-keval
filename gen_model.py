dc4936e451b6c214e1b8f78d045967b10e047647
eric <eric2digit@gmail.com>
2025-03-10 18:43:32 +0900

    hf dataset 로드하여 사용 | multi-turn 추론 (#1)

--- gen_model.py
+++ gen_model.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 from vllm import LLM, SamplingParams
 from tqdm import tqdm
+from datasets import load_dataset
 
 
 # Define a function to read from jsonl file
@@ -24,56 +25,42 @@ def write_jsonl(file_path, data, mode='a'):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("model", type=str, help="model name")
-    parser.add_argument('--data', type=str, default='data.jsonl')
+    parser.add_argument('--repo', type=str, default='twodigit/ko-bench')
+    parser.add_argument('--data', type=str, default='pairs_ko_question.jsonl')
     parser.add_argument('--num_samples', default=10000, type=int, help='num samples')
     parser.add_argument('--output', type=str, default='results/')
 
     args = parser.parse_args()
     print(args)
 
-    output_path = args.output
+    data_list = load_dataset(args.repo, data_files=args.data)['train'].to_list()
+    print(f'Load {args.repo}/{args.data}', '\n')
 
-    data_path = args.data.split('/')[-1].split('.jsonl')[0]
-    model_name = args.model.split('/')[-2] + '__' + args.model.split('/')[-1]
-    output_file_path = os.path.join(output_path, f"{data_path}__{model_name}.jsonl")
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    output_path = Path(args.output)
+    output_path.mkdir(parents=True, exist_ok=True) # 결과 디렉토리 생성
+    output_file_path = output_path / (args.model.split('/')[-1] + '__' + args.data.split('/')[-1].replace('.jsonl', '') + '__' + 'result.jsonl')
+    print('Output Path:', output_file_path, '\n')
 
     llm = LLM(model=args.model, max_model_len=4096)
-    llm.reset_prefix_cache()
     sampling_params = SamplingParams(max_tokens=2048, temperature=0.8)
 
-    data_list = read_jsonl(args.data)
-
-    system_prompt=""
-    is_first=True
-    for index, item in tqdm(enumerate(data_list)):
+    f = open(output_file_path, mode='w')
+    for index, item in tqdm(enumerate(data_list), total=len(data_list)):
         if index >= args.num_samples:
             break
-
+        
+        conversation = []
         for pair in item["pairs"]:
-            prompt =f"{system_prompt}\n질문:{pair['prompt']} "
-
-            conversation = [
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-
+            conversation.append({"role": "user", "content": pair['prompt']})
+            print(conversation)
             outputs = llm.chat(messages=conversation, sampling_params=sampling_params)
             answer = outputs[0].outputs[0].text
             pair["gen"] = answer
+            conversation.append({"role": "assistant", "content": answer})
 
-        if is_first:
-            mode='w'
-            is_first=False
-        else:
-            mode='a'
-        write_jsonl(output_file_path, item, mode)
-
-
+        f.write(json.dumps(item, ensure_ascii=False) + '\n')
+        f.flush()
+    f.close()
 
 
 if __name__ == '__main__':
